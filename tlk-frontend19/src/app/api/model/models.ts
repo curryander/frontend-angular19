537a2a26e67dd2c62f331ecb86a5f2ff7d52ed1f
Daniel <daniel.hohmann21@gmail.com>
2026-03-05 14:28:57 +0100

    auto-generated services

---
+++ tlk-frontend19/src/app/api/model/models.ts
@@ -0,0 +1,7 @@
+export * from './apiError';
+export * from './createVorgangResponse';
+export * from './jsonNullableString';
+export * from './pageExtractResponse';
+export * from './pageInfo';
+export * from './stapelProgress';
+export * from './vorgangWorkflowStatusResponse';
